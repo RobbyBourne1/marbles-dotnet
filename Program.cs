037d98163cc81aa01d6cfbcefe32fef898409668
Robby Bourne <robbybourne1@gmail.com>
2017-08-15 11:12:13 -0400

    Running total marbles

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace marbles_dotnet
 {
     class Program
     {
+        public class MarbleBag
+        {
+            public int NumberOfGreenMarbles { get; set; } = 0;
+            public int NumberOfBlueMarbles { get; set; } = 0;
+            public int NumberOfRedMarbles { get; set; } = 0; 
+        
+
+            public MarbleBag(int greenMarbles, int redMarbles, int blueMarbles)
+            {
+                NumberOfGreenMarbles = greenMarbles;
+                NumberOfBlueMarbles = blueMarbles;
+                NumberOfRedMarbles= redMarbles;
+            }
+
+            public override string ToString()
+            {
+                return $"I have {NumberOfGreenMarbles} green marbles, {NumberOfBlueMarbles} blue marbles, and {NumberOfRedMarbles} red marbles.";
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var marbles = new MarbleBag(5,6,7);
+            Console.WriteLine(marbles);
+
         }
     }
 }
